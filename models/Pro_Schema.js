f5fff42b0019cff8a095161739e7750e2b7d2869
hasham0 <hashamsaleem75@gmail.com>
2023-11-01 16:25:42 +0500

    update schemas

--- models/Pro_Schema.js
+++ models/Pro_Schema.js
@@ -2,38 +2,43 @@ import mongoose from "mongoose";
 const { Schema } = mongoose;
 
 // schema structure
-const prodSchema = new Schema({
-  title: {
-    type: String,
-    minLength: [5, "please provide atleast 5 characters"],
-    maxLength: [100],
-    require: [true, "please provide the title "],
-    unique: true,
-  },
-  price: {
-    type: Number,
-    min: [100, "alteast 100"],
-    max: [1000, "not greter than 1000"],
-    require: [true, "please provide the price"],
-  },
-  description: {
-    type: String,
-    require: [true, "please provide the description "],
-  },
-  category: {
-    type: String,
-    require: [true, "please provide "],
-  },
-  image: [
-    {
-      url: String,
+const prodSchema = new Schema(
+  {
+    title: {
+      type: String,
+      minLength: [5, "please provide atleast 5 characters"],
+      maxLength: [100],
+      require: [true, "please provide the title "],
+      unique: true,
     },
-  ],
-  rating: {
-    rate: { type: Number, min: [1, "alteast 1"], max: [5, "maximum 5"] },
-    count: { type: Number, min: [1], max: [10] },
+    price: {
+      type: Number,
+      min: [100, "alteast 100"],
+      max: [1000, "not greter than 1000"],
+      require: [true, "please provide the price"],
+    },
+    description: {
+      type: String,
+      require: [true, "please provide the description "],
+    },
+    category: {
+      type: String,
+      require: [true, "please provide "],
+    },
+    image: [
+      {
+        url: String,
+      },
+    ],
+    rating: {
+      rate: { type: Number, min: [1, "alteast 1"], max: [5, "maximum 5"] },
+      count: { type: Number, min: [1], max: [10] },
+    },
   },
-});
+  {
+    timestamps: true,
+  }
+);
 
 // pass schema in model
 export const Product_col =
--- models/Users_Schema.js
+++ models/Users_Schema.js
@@ -2,33 +2,42 @@ import mongoose from "mongoose";
 const { Schema } = mongoose;
 
 // schema structure
-const userSchema = new Schema({
-  first_name: {
-    type: String,
-    minLength: [3, "please provide atleast 3 characters"],
-    maxLength: [20],
-    require: [true, "please provide the first name"],
+const userSchema = new Schema(
+  {
+    first_name: {
+      type: String,
+      minLength: [3, "please provide atleast 3 characters"],
+      maxLength: [20],
+      require: [true, "please provide the first name"],
+    },
+    last_name: {
+      type: String,
+      minLength: [3, "please provide atleast 3 characters"],
+      maxLength: [20],
+      require: [true, "please provide the lastname name "],
+    },
+    email: {
+      type: String,
+      trim: true,
+      lowercase: true,
+      unique: true,
+      required: "Email address is required",
+      match: [
+        /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+        "Please fill a valid email address",
+      ],
+    },
+    password: {
+      type: String,
+      minLength: [5, "please provide atleast 5 characters"],
+      maxLength: [500],
+      require: [true, "please provide the password name "],
+    },
   },
-  last_name: {
-    type: String,
-    minLength: [3, "please provide atleast 3 characters"],
-    maxLength: [20],
-    require: [true, "please provide the lastname name "],
-  },
-  email: {
-    type: String,
-    minLength: [5, "please provide atleast 5 characters"],
-    maxLength: [30],
-    require: [true, "please provide the email name "],
-    unique: true,
-  },
-  password: {
-    type: String,
-    minLength: [5, "please provide atleast 5 characters"],
-    maxLength: [500],
-    require: [true, "please provide the password name "],
-  },
-});
+  {
+    timestamps: tue,
+  }
+);
 
 // pass schema in model
 export const Users_col =
